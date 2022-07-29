13a68cde855950693547a6e65e0c0fc5ab690fb6
_Ghost_ <56601374+duch3201@users.noreply.github.com>
2022-07-29 19:40:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,24 @@
 
 cfdisk /dev/sda
 
+read -p 'ROOT ' rootpath
+read -p 'SWAP ' swappath
+read -p 'BOOT ' swappath
+
+rootcmd='mkfs.ext4 -L ROOT /dev/sda2'
+swapcmd='mkswap -L SWAP /dev/sda1'
+bootcmd='mkfs.fat -F 32 /dev/sda4'
+
+rootpathcomplete='${rootcmd//[/dev/sda2]/rootcmd}'
+swapcmdpathcomplete='${swappath//[/dev/sda1]/swapcmd}'
+bootpathcomplete='${swappath//[/dev/sda4]/bootcmd}'
+
+echo rootpathcomplete
+echo swapcmdpathcomplete
+echo bootpathcomplete
+
+read -p "Press any key to resume ..."
+
 mkfs.ext4 -L BOOT /dev/sda1
 mkswap -L SWAP /dev/sda2  
 mkfs.fat -F 32 /dev/sda4
