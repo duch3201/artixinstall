c357e2289fb334cb1173f5845040f39e426eef23
_Ghost_ <56601374+duch3201@users.noreply.github.com>
2022-07-29 17:18:15 +0200

    Update artixinstall.py

--- artixinstall.py
+++ artixinstall.py
@@ -4,6 +4,9 @@ from secrets import choice
 #import platform
 import urllib.request
 import time
+import sys
+
+
 
 def connect():
     try:
@@ -23,129 +26,141 @@ def eth():
         print("retrying")
         eth()
 
-os.system("cfdisk")
+def part1():
+    os.system("cfdisk")
 
-root = input("ROOT: ")
-swap = input("SWAP: ")
-boot = input("BOOT: ")
+    root = input("ROOT: ")
+    swap = input("SWAP: ")
+    boot = input("BOOT: ")
 
-mkfs_root = "mkfs.ext4 -L ROOT "
-mkfs_boot = "mkfs.fat -F 32 "
-mkfs_swap = "mkswap -L SWAP "
+    mkfs_root = "mkfs.ext4 -L ROOT "
+    mkfs_boot = "mkfs.fat -F 32 "
+    mkfs_swap = "mkswap -L SWAP "
 
-print(mkfs_root + root)
-print(mkfs_boot + boot)
-print("fatlabel " + boot + " Boot")
-print(mkfs_swap + swap)
-
-
-os.system(mkfs_root + root)
-os.system(mkfs_boot + boot)
-os.system("fatlabel " + boot + " BOOT")
-os.system(mkfs_swap + swap)
-
-os.system("swapon /dev/disk/by-label/SWAP")
-os.system("mount /dev/disk/by-label/ROOT /mnt")
-os.system("mkdir /mnt/boot")
-os.system("mount /dev/disk/by-label/BOOT /mnt/boot")
-
-if connect() == True:
-    pass
-elif connect() == False:
-    #Has_internet_connection = False
-    print("you are not connected to the internet")
-    eth()
-
-print("installing base system...")
-time.sleep(1)
-os.system("basestrap /mnt base base-devel openrc elogind-openrc")
-
-print("installing the Kernel")
-print("1.linux")
-print("2.linux-zen")
-print("3.linux-lts")
-kernel_choice = input(":")
-if kernel_choice == "1":
-    os.system("basestrap /mnt linux linux-firmware")
-if kernel_choice == "2":
-    os.system("basestrap /mnt linux-zen linux-firmware")
-    print("test")
-if kernel_choice == "3":
-    os.system("basestrap /mnt linux-lts linux-firmware")
-
-os.system("fstabgen -U /mnt >> /mnt/etc/fstab")
-
-os.system("artix-chroot /mnt")
-
-os.system("ln -sf /usr/share/zoneinfo/Region/City /etc/localtime")
-
-os.system("hwclock --systohc")
-
-os.system("pacman -S nano")
-
-os.system("nano /etc/locale.gen")
-
-os.system("locale-gen")
+    print(mkfs_root + root)
+    print(mkfs_boot + boot)
+    print("fatlabel " + boot + " Boot")
+    print(mkfs_swap + swap)
 
 
-print("installing the bootloader")
-time.sleep(1)
-os.system("pacman -S grub os-prober efibootmgr")
-#os.system("grub-install --recheck /dev/sda") for bios systems
-os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub")
-os.system("grub-mkconfig -o /boot/grub/grub.cfg")
+    os.system(mkfs_root + root)
+    os.system(mkfs_boot + boot)
+    os.system("fatlabel " + boot + " BOOT")
+    os.system(mkfs_swap + swap)
 
-print("adding users")
-usrname = input("Username: ")
-os.system("useradd -m " + usrname)
-os.system("passwd " + usrname)
+    os.system("swapon /dev/disk/by-label/SWAP")
+    os.system("mount /dev/disk/by-label/ROOT /mnt")
+    os.system("mkdir /mnt/boot")
+    os.system("mount /dev/disk/by-label/BOOT /mnt/boot")
 
-print("set root password")
-os.system("passwd")
+    if connect() == True:
+        pass
+    elif connect() == False:
+        #Has_internet_connection = False
+        print("you are not connected to the internet")
+        eth()
 
-print("set Hostname")
-hostname = input(": ")
-with open("/etc/hostname", 'w') as hostname_file:
-    hostname_file.write(hostname)
+    print("installing base system...")
+    time.sleep(1)
+    os.system("basestrap /mnt base base-devel openrc elogind-openrc")
 
-with open("/etc/hosts", 'w') as hosts:
-    data = ""
-    data[3] = "127.0.0.1       localhost"
-    data[4] = "::1             localhost"
-    data[5] = "127.0.1.1       " + usrname + "." + hostname + "  " + hostname
-    hosts.writelines( data )
+    print("installing the Kernel")
+    print("1.linux")
+    print("2.linux-zen")
+    print("3.linux-lts")
+    kernel_choice = input(":")
+    if kernel_choice == "1":
+        os.system("basestrap /mnt linux linux-firmware")
+    if kernel_choice == "2":
+        os.system("basestrap /mnt linux-zen linux-firmware")
+        print("test")
+    if kernel_choice == "3":
+        os.system("basestrap /mnt linux-lts linux-firmware")
 
-print("installing dhcp")
-os.system("pacman -S dhclient")
+    os.system("fstabgen -U /mnt >> /mnt/etc/fstab")
+    
+    print("please run artix-chroot and re start this script")
+    
+
+#os.system("artix-chroot /mnt")
+
+def part2():
+    os.system("ln -sf /usr/share/zoneinfo/Region/City /etc/localtime")
+
+    os.system("hwclock --systohc")
+
+    os.system("pacman -S nano")
+
+    os.system("nano /etc/locale.gen")
+
+    os.system("locale-gen")
 
 
-print("installing connman")
-os.system("pacman -S connman-openrc connman-gtk")
-os.system("rc-update add connmand")
+    print("installing the bootloader")
+    time.sleep(1)
+    os.system("pacman -S grub os-prober efibootmgr")
+    #os.system("grub-install --recheck /dev/sda") for bios systems
+    os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub")
+    os.system("grub-mkconfig -o /boot/grub/grub.cfg")
 
-print("installing xorg")
-os.system("pacman -S xorg")
+    print("adding users")
+    usrname = input("Username: ")
+    os.system("useradd -m " + usrname)
+    os.system("passwd " + usrname)
 
-print("installing the desktop enviroment")
-print("1. kde 2.mate 3.gnome 4.xfce4 5.lxqt")
-usr_choice_de = input(": ")
-if usr_choice_de == "1":
-    os.system("pacman -S plasma kde-applications")
-elif usr_choice_de == "2":
-    os.system("pacman -S mate mate-extra system-config-printer blueman connman-gtk")
-elif usr_choice_de == "3":
-    os.system("pacman -S gnome")
-elif usr_choice_de == "4":
-    os.system("pacman -S xfce4 xfce4-goodies")
-elif usr_choice_de == "5":
-    os.system("pacman -S lxqt")
+    print("set root password")
+    os.system("passwd")
 
-print("downloading display manager")
-os.system("pacman -S sddm-openrc or sddm-runit or sddm-s6 or sddm-suite66")
+    print("set Hostname")
+    hostname = input(": ")
+    with open("/etc/hostname", 'w') as hostname_file:
+        hostname_file.write(hostname)
 
-print("installing usr packages")
-os.system("pacman -S firefox")
-os.system("pacman -S visual-studio-code")
+    with open("/etc/hosts", 'w') as hosts:
+        data = ""
+        data[3] = "127.0.0.1       localhost"
+        data[4] = "::1             localhost"
+        data[5] = "127.0.1.1       " + usrname + "." + hostname + "  " + hostname
+        hosts.writelines( data )
 
-input("installation complete! press any key continue")
-os.system("sudo reboot")
+    print("installing dhcp")
+    os.system("pacman -S dhclient")
+
+
+    print("installing connman")
+    os.system("pacman -S connman-openrc connman-gtk")
+    os.system("rc-update add connmand")
+
+    print("installing xorg")
+    os.system("pacman -S xorg")
+
+    print("installing the desktop enviroment")
+    print("1. kde 2.mate 3.gnome 4.xfce4 5.lxqt")
+    usr_choice_de = input(": ")
+    if usr_choice_de == "1":
+        os.system("pacman -S plasma kde-applications")
+    elif usr_choice_de == "2":
+        os.system("pacman -S mate mate-extra system-config-printer blueman connman-gtk")
+    elif usr_choice_de == "3":
+        os.system("pacman -S gnome")
+    elif usr_choice_de == "4":
+        os.system("pacman -S xfce4 xfce4-goodies")
+    elif usr_choice_de == "5":
+        os.system("pacman -S lxqt")
+
+    print("downloading display manager")
+    os.system("pacman -S gdm-openrc")
+    os.system("rc-update add gdm-openrc"
+
+    print("installing usr packages")
+    os.system("pacman -S firefox")
+    os.system("pacman -S visual-studio-code")
+
+    input("installation complete! press any key continue")
+    os.system("sudo reboot")
+
+
+if sys.argv[0] == "part2":
+    part2()
+else:
+    part1()
